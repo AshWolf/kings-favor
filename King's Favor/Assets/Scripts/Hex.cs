e321930c7450d638fe08ce088805c4d89fa0ac4f
Nolan Kuza <nkuza8@gmail.com>
2024-12-31 22:25:39 -0500

    Add comparison to Hex

--- King's Favor/Assets/Scripts/Hex.cs
+++ King's Favor/Assets/Scripts/Hex.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 [System.Serializable]
-public struct Hex
+public struct Hex : IEquatable<Hex>
 {
     public int q;
     public int r;
@@ -15,7 +15,7 @@ public struct Hex
     [SerializeField]
     public int s { get => -q - r; }
 
-    private static Hex[] directions = {
+    public readonly static Hex[] Directions = {
         new(+1, 0), new(+1, -1), new(0, -1),
         new(-1, 0), new(-1, +1), new(0, +1),
     };
@@ -49,16 +49,16 @@ public struct Hex
         return (a - b).Magnitude();
     }
 
-    public Vector2 ToPixel(float size=0.5f)
+    public Vector2 ToPixel(float size = 0.5f)
     {
-        var x = 3f/2 * q;
-        var y = Mathf.Sqrt(3)/2 * q + Mathf.Sqrt(3) * r;
+        var x = 3f / 2 * q;
+        var y = Mathf.Sqrt(3) / 2 * q + Mathf.Sqrt(3) * r;
         return size * (new Vector2(x, y));
     }
 
-    public static Hex PixelToHex(Vector2 pixel, float size=0.5f) {
+    public static Hex PixelToHex(Vector2 pixel, float size = 0.5f) {
         var q = (2f / 3 * pixel.x) / size;
-        var r = (-1f/3 * pixel.x + Mathf.Sqrt(3)/3 * pixel.y) / size;
+        var r = (-1f / 3 * pixel.x + Mathf.Sqrt(3) / 3 * pixel.y) / size;
 
         return RoundHex(q, r);
     }
@@ -93,14 +93,14 @@ public struct Hex
 
     public static IEnumerable<Hex> Ring(int radius)
     {
-        if(radius == 0)
+        if (radius == 0)
         {
             yield return Hex.Zero;
             yield break;
         }
 
-        Hex hex = directions[4] * radius;
-        foreach (Hex dir in directions)
+        Hex hex = Directions[4] * radius;
+        foreach (Hex dir in Directions)
         {
             foreach (var _ in Enumerable.Range(0, radius))
             {
@@ -110,29 +110,30 @@ public struct Hex
         }
     }
 
+    public override bool Equals(object other) {
+        if (other is not Hex)
+        {
+            return false;
+        }
 
-    public static Hex operator +(Hex a, Hex b)
-    {
-        return new Hex(a.q + b.q, a.r + b.r);
+        return Equals((Hex) other);
     }
 
-    public static Hex operator -(Hex a, Hex b)
-    {
-        return new Hex(a.q - b.q, a.r - b.r);
-    }
+    public bool Equals(Hex other) => q == other.q && r == other.r;
 
-    public static Hex operator -(Hex a)
-    {
-        return new Hex(-a.q, -a.r);
-    }
+    public override int GetHashCode() => (q, r).GetHashCode();
 
-    public static Hex operator *(int scalar, Hex hex)
-    {
-        return new Hex(scalar * hex.q, scalar * hex.r);
-    }
-    public static Hex operator *(Hex hex, int scalar)
-    {
-        return new Hex(scalar * hex.q, scalar * hex.r);
-    }
+    public static bool operator ==(Hex a, Hex b) => a.Equals(b);
 
+    public static bool operator !=(Hex a, Hex b) => !(a == b);
+
+    public static Hex operator +(Hex a, Hex b) => new(a.q + b.q, a.r + b.r);
+
+    public static Hex operator -(Hex a, Hex b) => new(a.q - b.q, a.r - b.r);
+
+    public static Hex operator -(Hex a) => new Hex(-a.q, -a.r);
+
+    public static Hex operator *(int scalar, Hex hex) => new(scalar * hex.q, scalar * hex.r);
+
+    public static Hex operator *(Hex hex, int scalar) => new(scalar * hex.q, scalar * hex.r);
 }
