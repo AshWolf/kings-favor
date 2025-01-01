942270907b37439172665558dd7dfd72cb9325b4
Nolan Kuza <nkuza8@gmail.com>
2024-12-31 22:25:49 -0500

    Add hex range calculation

--- King's Favor/Assets/Scripts/Map.cs
+++ King's Favor/Assets/Scripts/Map.cs
@@ -23,7 +23,10 @@ public class Map : MonoBehaviour
 
     public List<Wall> Walls = new();
 
-    public Dictionary<Hex, Tile> Tiles = new();
+    public float HexSize = 0.51f;
+    public int Radius = 5;
+
+    private Dictionary<Hex, Tile> Tiles = new();
 
     // Start is called before the first frame update
     void Start()
@@ -45,29 +48,63 @@ public class Map : MonoBehaviour
         var plane = new Plane(transform.up, transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        //Initialise the enter variable
-        float enter = 0.0f;
-
-        if (plane.Raycast(ray, out enter))
+        if (plane.Raycast(ray, out float enter))
         {
-            //Get the point that is clicked
             Vector3 hitPoint = ray.GetPoint(enter);
 
             hitPoint = transform.InverseTransformPoint(hitPoint);
 
-            Hex hex = Hex.PixelToHex(new(hitPoint.x, hitPoint.z), size: 0.51f);
+            Hex hex = Hex.PixelToHex(new(hitPoint.x, hitPoint.z), size: HexSize);
 
-            if (hex.Magnitude() <= 5)
+            if (IsHexValid(hex))
             {
-                Debug.Log(hex);
-                var obj = Tiles[hex];
-                obj.GetComponent<MeshRenderer>().material.color = Color.red;
-            }
-            else
-            {
-                Debug.Log("None");
+                // TODO: currently is just a demo to highlight hexes within a range of 1
+                foreach (Hex hexInRange in GetHexesInRange(hex, 1))
+                {
+                    var obj = Tiles[hexInRange];
+                    obj.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
             }
         }
+    }
+
+    private HashSet<Hex> GetHexesInRange(Hex center, int range)
+    {
+        HashSet<Hex> visited = new();
+        List<Hex> queue = new();
+
+        visited.Add(center);
+        queue.Add(center);
+
+        for (int i = 0; i < range; i++)
+        {
+            List<Hex> nextQueue = new();
+            foreach (Hex hex in queue)
+            {
+                foreach (Hex direction in Hex.Directions)
+                {
+                    Hex candidate = hex + direction;
+                    if (!IsHexValid(candidate) && !visited.Contains(candidate) && !HasWall(hex, candidate))
+                    {
+                        visited.Add(candidate);
+                        nextQueue.Add(candidate);
+                    }
+                }
+            }
+            queue = nextQueue;
+        }
+
+        return visited;
+    }
+
+    private bool HasWall(Hex a, Hex b)
+    {
+        return Walls.Any(wall => (wall.First == a && wall.Second == b) || (wall.First == b && wall.Second == a));
+    }
+
+    private bool IsHexValid(Hex hex)
+    {
+        return hex.Magnitude() <= Radius;
     }
 }
 
@@ -76,13 +113,12 @@ public class MapEditor : Editor
 {
     private void GenerateMap(Map map)
     {
-        float hexSize = 0.51f;
 
-        for (int radius = 0; radius <= 5; radius++)
+        for (int radius = 0; radius <= map.Radius; radius++)
         {
             foreach (Hex hex in Hex.Ring(radius))
             {
-                var pixel = hex.ToPixel(hexSize);
+                var pixel = hex.ToPixel(map.HexSize);
                 var obj = GameObject.Instantiate(map.HexPrefab, map.transform);
                 obj.name = hex.ToString();
                 obj.transform.localPosition = new(pixel.x, 0, pixel.y);
@@ -92,8 +128,8 @@ public class MapEditor : Editor
 
         foreach (Wall wall in map.Walls)
         {
-            var p1 = wall.First.ToPixel(hexSize);
-            var p2 = wall.Second.ToPixel(hexSize);
+            var p1 = wall.First.ToPixel(map.HexSize);
+            var p2 = wall.Second.ToPixel(map.HexSize);
             var center = (p1 + p2) / 2;
             var perp = p2 - p1;
 
